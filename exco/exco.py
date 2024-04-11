b4e9c0feacfb583665d39f88b306b58b792aaefd
Luc <luc@aggett.com>
2024-04-11 20:19:32 +0200

    add file upload and overview page

--- exco/exco.py
+++ exco/exco.py
@@ -1,12 +1,69 @@
-import flask
-from flask import render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, FileField, TextAreaField, BooleanField, IntegerField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileRequired
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
+app.debug = True
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+app.secret_key = 'super secret'
+app.config['UPLOAD_FOLDER'] = 'exco/static/data'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'exco.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
+class UploadForm(FlaskForm):
+    file = FileField('File', validators=[FileRequired()])
+    uploader = StringField('Uploader Name', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    year = IntegerField('Year', default=2024)
+    course = StringField('Course')
+    has_solution = BooleanField('Has Solution')
+
+class Resource(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(128), nullable=False)
+    uploader = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(256), nullable=True)
+    year = db.Column(db.Integer, nullable=True)
+    course = db.Column(db.String(128), nullable=True)
+    has_solution = db.Column(db.Boolean, nullable=False)
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+
+# Page Routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    resources = Resource.query.all()  # Query all resources from the database
+    return render_template('index.html', resources=resources)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():
+    form = UploadForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        uploader = form.uploader.data
+        description = form.description.data
+        filename = file.filename
+        year = form.year.data
+        course = form.course.data
+        has_solution = form.has_solution.data
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        resource = Resource(filename=filename, uploader=uploader, description=description, year=year, course=course, has_solution=has_solution)
+        db.session.add(resource)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('upload.html', form=form)
 
 
 if __name__ == '__main__':
