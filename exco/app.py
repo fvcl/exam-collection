885cda995f3abe1ce4cae2c5bf0294a6c25c2a33
Luc <luc@aggett.com>
2024-04-16 00:52:25 +0200

    cleanup app initialisation + documentation

--- exco/app.py
+++ exco/app.py
@@ -1,16 +1,17 @@
+"""
+This is the main application file for the Flask web application.
+It initializes the Flask app, configures the database, and creates the database tables and imports the routes.
+"""
+
 import random
 import os
 from flask import Flask
 
 try:
     from exco.extensions import db
-    from exco.forms import UploadForm
-    from exco.models import Resource
     from exco.utils import format_date_info
 except ImportError:
     from extensions import db
-    from forms import UploadForm
-    from models import Resource
     from utils import format_date_info
 
 app = Flask(__name__)
@@ -30,33 +31,32 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app.secret_key = random.randbytes(16)
 app.config['UPLOAD_FOLDER'] = 'static/data'
 
-if DEVELOPMENT_MODE is not True:
+if DEVELOPMENT_MODE is True:
+    # Development mode configuration
+    print(format_date_info("BOOT"), "Running in development mode with SQLite database.")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'db', 'exam-collection.db')
+else:
     # Production mode configuration
     app.config['SERVER_NAME'] = 'exams.fvcl.ch'
     app.config['PREFERRED_URL_SCHEME'] = 'https'
-    app.config['APPLICATION_ROOT'] = '/'
     app.config['SESSION_COOKIE_SECURE'] = True
-
-# Configure the database
-if DEVELOPMENT_MODE is True:
-    print(format_date_info("BOOT"),
-          "Running in development mode with SQLite database.")
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'db', 'exam-collection.db')
-else:
-    print(format_date_info("BOOT"),
-          "Running in production mode with PostgreSQL database.")
+    print(format_date_info("BOOT"), "Running in production mode with PostgreSQL database.")
     app.config[
         'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ac69327d785a127a800e@diy-prod_exam-collection-db:5432/diy-prod'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['APPLICATION_ROOT'] = '/'
 
 # Initialize the database
 db.init_app(app)
 
 # Create the database tables (if they don't exist)
 with app.app_context():
+    # If the database is not created, create it
     db.create_all()
-    print(format_date_info("BOOT"), "Created database tables.")
+    print(format_date_info("BOOT"), "Created database tables (if they didn't exist yet).")
+
+    # Import the routes (requires the active app context)
     try:
         import exco.routes
     except ImportError:
